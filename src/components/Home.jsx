7d8169de7e8e0ee8a22a905c2277d22d20724427
devsilva <jersonsilva99@hotmail.com>
2022-10-27 12:22:30 -0500

    refactor: Import imgslider and fix stlyes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import ImgSlider from './ImgSlider'
 // 3:20.30
 
 const Home = () => {
   return <>
-    <Content>Home</Content>
+    <Content>
+      <ImgSlider />
+    </Content>
   </>
 }
 
 export default Home
 
 const Content = styled.main`
-  min-height: calc(100vh - 78px);
+  z-index: 1;
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
+  min-height: calc(100vh - 78px);
+
 
   &::before {
-    background: url('/images/home-background.png') center center / cover no-repeat fixed;
-    content: '';
-    position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: 1;
+    content: '';
+    z-index: -1;
+    position: absolute;
+    background: url('/images/home-background.png') center center / cover no-repeat fixed;
   }
 `
