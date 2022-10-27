3c4eab424debac3fb3ac996dd2449d3f0f8c964d
devsilva <jersonsilva99@hotmail.com>
2022-10-26 22:51:17 -0500

    style: Import React and add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import React from 'react'
+
 import Header from './components/Header'
 import Home from './components/Home'
 
 const App = () => {
   return <>
-      <div>
-        <Header />
-        <Home />
-    </div>
+    <Header />
+    <Home />
   </>
 }
 
